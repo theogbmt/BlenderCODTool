4060a9271444d98209c8fd673ac6968151959ee1
theogbmt <139086027+theogbmt@users.noreply.github.com>
2024-07-04 16:27:41 +0100

    Update __init__.py

--- BlenderCODTool/__init__.py
+++ BlenderCODTool/__init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "BlenderCODTool 4.1",
     "author": "( 4.1 ) bigmanting ( 3.0 ) Ma_rv ( 2.8 ) CoDEmanX, Flybynyt, SE2Dev",
-    "version": (0, 9, 3),
+    "version": (0, 9, 0),
     "blender": (4, 1, 0),
     "location": "File > Import  |  File > Export",
     "description": "Import/Export XModels and XAnims",
@@ -37,7 +37,7 @@ import bpy
 import os
 import importlib
 
-folder_blacklist = ["__pycache__", "blendercodtool_updater"]
+folder_blacklist = ["__pycache__"]
 file_blacklist = ["__init__.py", "addon_updater_ops", "addon_updater.py"]
 
 addon_folders = list([__path__[0]])
@@ -97,23 +97,13 @@ def AlxUnregisterClassQueue():
 ##################################################
 
 from .BCT_utilities import submenu_reloader_register, submenu_reloader_unregister
-from . import addon_updater_ops
 
 def register():
-    try:
-        addon_updater_ops.register(bl_info)
-    except:
-        pass
-
     AlxRegisterClassQueue()
 
     submenu_reloader_register()
 
-    bpy.context.preferences.use_preferences_save = True
-
 def unregister():
-    addon_updater_ops.unregister()
-
     AlxUnregisterClassQueue()
 
     submenu_reloader_unregister()
